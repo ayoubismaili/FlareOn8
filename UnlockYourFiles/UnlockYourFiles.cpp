5cf326b1dd40f250ff66ed5cb3d4456a8dc61b57
ayoubismaili <ayoubismaili1@gmail.com>
2022-05-30 00:44:34 +0100

    Full working code for UnlockYourFiles.

--- UnlockYourFiles/UnlockYourFiles.cpp
+++ UnlockYourFiles/UnlockYourFiles.cpp
@@ -28,33 +28,48 @@ HANDLE hConsoleOutput;
 CHAR PathName[] = "Files";
 CHAR FileName[] = "*.encrypted";
 
+#define SOURCE_FILE_EXTENSION_LENGTH 10
+#define SOURCE_FILE_EXTENSION_LENGTH_NO_DOT (SOURCE_FILE_EXTENSION_LENGTH-1)
+
+#define ROTL_BYTE(value,c) (((value)<<(c)) | ((value)>>(8-(c))))
+
 // sub_401070 : Ransom_IntToStr
 CHAR* Ransom_IntToStr(INT Value, CHAR* Destination)
 {
     if (Value >= 10)
         Destination = (CHAR*)Ransom_IntToStr(Value / 10, Destination);
+    //Convert a digit to ASCII value
     Destination[0] = Value % 10 + '0';
     Destination[1] = 0;
     return Destination + 1;
 }
 
 // sub_4010C0 : Ransom_HandleError
-void Ransom_HandleError(const CHAR* lpBuffer)
+void Ransom_HandleError(const CHAR* ErrorMessage)
 {
-    DWORD LastError; // eax
-    DWORD v2; // eax
-    DWORD v3; // eax
-    char Buffer[12]; // [esp+0h] [ebp-Ch] BYREF
+    DWORD LastError;
+    DWORD ErrorMessageLength;
+    DWORD LastErrorLength;
+    CHAR LastErrorBuffer[12];
 
+    //Get last error
     LastError = GetLastError();
-    Ransom_IntToStr(LastError, Buffer);
-    v2 = strlen(lpBuffer);
-    WriteConsoleA(hConsoleOutput, lpBuffer, v2, NULL, NULL);
+    //Convert last error value to string
+    Ransom_IntToStr(LastError, LastErrorBuffer);
+    //Get string length of error message
+    ErrorMessageLength = strlen(ErrorMessage);
+    //Write error message to the Console
+    WriteConsoleA(hConsoleOutput, ErrorMessage, ErrorMessageLength, NULL, NULL);
+    //Write failed error message to the Console
     WriteConsoleA(hConsoleOutput, FailedError, FAILED_ERROR_LENGTH, NULL, NULL);
-    v3 = strlen(Buffer);
-    WriteConsoleA(hConsoleOutput, Buffer, v3, NULL, NULL);
+    //Get string length of last error
+    LastErrorLength = strlen(LastErrorBuffer);
+    //Write last error string to the Console
+    WriteConsoleA(hConsoleOutput, LastErrorBuffer, LastErrorLength, NULL, NULL);
+    //Write files directory message to the Console
     WriteConsoleA(hConsoleOutput, FilesDirectory, FILES_DIRECTORY_LENGTH, NULL, NULL);
-    ExitProcess(1u);
+    //Exit the process
+    ExitProcess(1);
 }
 
 // sub_401030: Ransom_StrCpy
@@ -62,6 +77,7 @@ int Ransom_StrCpy(CHAR* Destination, CHAR* Source)
 {
     int i;
 
+    //Copy string
     for (i = 0; ; ++i)
     {
         Destination[i] = Source[i];
@@ -71,51 +87,66 @@ int Ransom_StrCpy(CHAR* Destination, CHAR* Source)
     return i;
 }
 
-char sub_4011F0(CHAR* a1, CHAR* a2)
+// sub_4011F0 : Ransom_DecryptDataBlock
+VOID Ransom_DecryptDataBlock(UCHAR* DataBlock, UCHAR* Key)
 {
-    int i; // ecx
-    CHAR result; // al
+    UINT i;
+    UCHAR Value;
 
-    for (i = 0; (char)i < 8; i++)
+    //Decrypt one block of data
+    for (i = 0; i < 8; i++)
     {
-        result = _rotl(a2[i] ^ a1[i], i) - i;
-        a1[i] = result;
+        Value = ROTL_BYTE(Key[i] ^ DataBlock[i], i) - i;
+        DataBlock[i] = Value;
     }
-    return result;
 }
 
 // sub_401220 : Ransom_DecryptFile
-VOID Ransom_DecryptFile(CHAR* lpFileName, CHAR* a2, CHAR* a3)
+VOID Ransom_DecryptFile(CHAR* Source, CHAR* Destination, UCHAR* Key)
 {
-    CHAR Buffer[8]; // [esp+0h] [ebp-18h] BYREF
-    DWORD NumberOfBytesWritten; // [esp+8h] [ebp-10h] BYREF
-    HANDLE hFile; // [esp+Ch] [ebp-Ch]
-    HANDLE hObject; // [esp+10h] [ebp-8h]
-    DWORD NumberOfBytesRead; // [esp+14h] [ebp-4h] BYREF
+    UCHAR DataBlock[8];
+    DWORD NumberOfBytesWritten;
+    HANDLE hSourceFile;
+    HANDLE hDestinationFile;
+    DWORD NumberOfBytesRead;
     
-    hFile = CreateFileA(lpFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE)
-        Ransom_HandleError(lpFileName);
-    hObject = CreateFileA(a2, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hObject == INVALID_HANDLE_VALUE)
-        Ransom_HandleError(a2);
+    //Open source file (encrypted file)
+    hSourceFile = CreateFileA(Source, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hSourceFile == INVALID_HANDLE_VALUE)
+        Ransom_HandleError(Source);
+    //Open destination file (target file)
+    hDestinationFile = CreateFileA(Destination, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hDestinationFile == INVALID_HANDLE_VALUE)
+        Ransom_HandleError(Destination);
+    //Decryption loop
     while (1)
     {
-        if (!ReadFile(hFile, Buffer, BLOCK_SIZE, &NumberOfBytesRead, NULL))
-            Ransom_HandleError(lpFileName);
+        //Read one block of data from source file
+        if (!ReadFile(hSourceFile, DataBlock, BLOCK_SIZE, &NumberOfBytesRead, NULL))
+            Ransom_HandleError(Source);
+        //Check if there is more data
         if (!NumberOfBytesRead)
             break;
-        sub_4011F0(Buffer, a3);
-        if (!WriteFile(hObject, Buffer, NumberOfBytesRead, &NumberOfBytesWritten, NULL))
-            Ransom_HandleError(a2);
+        //Decrypt one block of data
+        Ransom_DecryptDataBlock(DataBlock, Key);
+        //Write one block of data to destination file
+        if (!WriteFile(hDestinationFile, DataBlock, NumberOfBytesRead, &NumberOfBytesWritten, NULL))
+            Ransom_HandleError(Destination);
     }
-    CloseHandle(hObject);
-    CloseHandle(hFile);
-    NumberOfBytesRead = strlen(lpFileName);
-    a2[NumberOfBytesRead - 10] = 10;
-    WriteConsoleA(hConsoleOutput, lpFileName, NumberOfBytesRead, NULL, NULL);
+    //Close destination file
+    CloseHandle(hDestinationFile);
+    //Close source file
+    CloseHandle(hSourceFile);
+    //Get source file name length
+    NumberOfBytesRead = strlen(Source);
+    //Modify destination file name
+    Destination[NumberOfBytesRead - SOURCE_FILE_EXTENSION_LENGTH] = '\n';
+    //Write source file name to the Console
+    WriteConsoleA(hConsoleOutput, Source, NumberOfBytesRead, NULL, NULL);
+    //Write arrow to the Console
     WriteConsoleA(hConsoleOutput, Arrow, ARROW_LENGTH, NULL, NULL);
-    WriteConsoleA(hConsoleOutput, a2, NumberOfBytesRead - 9, NULL, NULL);
+    //Write modified destination file name to the Console
+    WriteConsoleA(hConsoleOutput, Destination, NumberOfBytesRead - SOURCE_FILE_EXTENSION_LENGTH_NO_DOT, NULL, NULL);
 }
 
 // sub_401160 : Ransom_PrintTotal
@@ -125,19 +156,25 @@ VOID Ransom_PrintTotal(INT TotalFiles)
     char NumberBuffer[12];
     DWORD nNumberOfCharsToWrite;
     
+    //Convert total files to String
     Ransom_IntToStr(TotalFiles, NumberBuffer);
+    //Write string to Console
     WriteConsoleA(hConsoleOutput, NumberOfFiles, NUMBER_OF_FILES_LENGTH, NULL, NULL);
+    //Count number of caracters in number buffer
     NumberLength = strlen(NumberBuffer);
+    //Write total files to the Console
     WriteConsoleA(hConsoleOutput, NumberBuffer, NumberLength, NULL, NULL);
+    //Print new line only if the total files is non-zero, otherwise print the complete message
     if (TotalFiles)
         nNumberOfCharsToWrite = ONLY_NEWLINE_LENGTH;
     else
         nNumberOfCharsToWrite = FILES_DIRECTORY_LENGTH;
+    //Write string to the Console
     WriteConsoleA(hConsoleOutput, FilesDirectory, nNumberOfCharsToWrite, NULL, NULL);
 }
 
 // sub_401370 : Ransom_DecryptFiles
-BOOL Ransom_DecryptFiles(CHAR Key[8]) {
+BOOL Ransom_DecryptFiles(UCHAR* Key) {
     struct _WIN32_FIND_DATAA FindFileData;
     CHAR cFileName[64];
     INT FileNameLength;
@@ -157,7 +194,8 @@ BOOL Ransom_DecryptFiles(CHAR Key[8]) {
     {
         //Copy file name to the local buffer and return the length of the file name
         FileNameLength = Ransom_StrCpy(cFileName, FindFileData.cFileName);
-        FindFileData.cAlternateFileName[FileNameLength + 6] = 0;
+        //Remove extension from destination file
+        cFileName[FileNameLength - SOURCE_FILE_EXTENSION_LENGTH] = 0;
         //Decrypt one file to the new file name using the specified key
         Ransom_DecryptFile(FindFileData.cFileName, cFileName, Key);
         //Increment file total
@@ -176,7 +214,7 @@ BOOL Ransom_DecryptFiles(CHAR Key[8]) {
 int main()
 {
     UINT uExitCode;
-    CHAR Key[9];
+    UCHAR Key[9];
     DWORD NumberOfCharsRead;
 
     //Fill Key Buffer with Zeros
