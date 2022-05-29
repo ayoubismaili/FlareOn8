804c7565d6dad05b128e5a6630b01e3e53040e55
ayoubismaili <ayoubismaili1@gmail.com>
2022-05-29 20:42:44 +0100

    Initial commit

--- UnlockYourFiles/UnlockYourFiles.cpp
+++ UnlockYourFiles/UnlockYourFiles.cpp
@@ -36,7 +36,8 @@ CHAR* sub_401070(unsigned int a1, CHAR* a2)
     return a2 + 1;
 }
 
-void sub_4010C0(const CHAR* lpBuffer)
+// sub_4010C0 : Ransom_HandleError
+void Ransom_HandleError(const CHAR* lpBuffer)
 {
     DWORD LastError; // eax
     DWORD v2; // eax
@@ -90,19 +91,19 @@ VOID sub_401220(CHAR* lpFileName, CHAR* a2, CHAR* a3)
     
     hFile = CreateFileA(lpFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hFile == INVALID_HANDLE_VALUE)
-        sub_4010C0(lpFileName);
+        Ransom_HandleError(lpFileName);
     hObject = CreateFileA(a2, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hObject == INVALID_HANDLE_VALUE)
-        sub_4010C0(a2);
+        Ransom_HandleError(a2);
     while (1)
     {
         if (!ReadFile(hFile, Buffer, BLOCK_SIZE, &NumberOfBytesRead, NULL))
-            sub_4010C0(lpFileName);
+            Ransom_HandleError(lpFileName);
         if (!NumberOfBytesRead)
             break;
         sub_4011F0(Buffer, a3);
         if (!WriteFile(hObject, Buffer, NumberOfBytesRead, &NumberOfBytesWritten, NULL))
-            sub_4010C0(a2);
+            Ransom_HandleError(a2);
     }
     CloseHandle(hObject);
     CloseHandle(hFile);
@@ -130,7 +131,8 @@ VOID sub_401160(unsigned int a1)
     WriteConsoleA(hConsoleOutput, FilesDirectory, nNumberOfCharsToWrite, NULL, NULL);
 }
 
-BOOL sub_401370(CHAR Buffer[9]) {
+// sub_401370 : Ransom_DecryptFiles
+BOOL Ransom_DecryptFiles(CHAR Key[8]) {
     struct _WIN32_FIND_DATAA FindFileData; // [esp+0h] [ebp-190h] BYREF
     CHAR v3[64]; // [esp+140h] [ebp-50h] BYREF
     int v4; // [esp+180h] [ebp-10h]
@@ -139,38 +141,45 @@ BOOL sub_401370(CHAR Buffer[9]) {
 
     v6 = 0;
     if (!SetCurrentDirectoryA(PathName))
-        sub_4010C0("SetCurrentDirectory(\"Files\")");
+        Ransom_HandleError("SetCurrentDirectory(\"Files\")");
     hFindFile = FindFirstFileA(FileName, &FindFileData);
     if (hFindFile == INVALID_HANDLE_VALUE)
-        sub_4010C0("FindFirstFile");
+        Ransom_HandleError("FindFirstFile");
     do
     {
         v4 = sub_401030(v3, FindFileData.cFileName);
         FindFileData.cAlternateFileName[v4 + 6] = 0;
-        sub_401220(FindFileData.cFileName, v3, Buffer);
+        sub_401220(FindFileData.cFileName, v3, Key);
         ++v6;
     } while (FindNextFileA(hFindFile, &FindFileData));
     if (GetLastError() != ERROR_NO_MORE_FILES)
-        sub_4010C0("FindNextFile");
+        Ransom_HandleError("FindNextFile");
     sub_401160(v6);
     return v6 != 0;
 }
 
+// start @ 00401460 : main
 int main()
 {
-    UINT v0;
-    CHAR Buffer[9];
+    UINT uExitCode;
+    CHAR Key[9];
     DWORD NumberOfCharsRead;
 
-    ZeroMemory(&Buffer, sizeof(Buffer));
+    //Fill Key Buffer with Zeros
+    ZeroMemory(&Key, sizeof(Key));
+    //Obtain Standard Input Handle
     hConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
+    //Obtain Standard Output Handle
     hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    //Customize the colors of the Console
     SetConsoleTextAttribute(hConsoleOutput, FOREGROUND_GREEN | FOREGROUND_RED
         | FOREGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_INTENSITY);
+    //Write the message to the Console
     WriteConsoleA(hConsoleOutput, Message, MESSAGE_LENGTH, NULL, NULL);
-    ReadConsoleA(hConsoleInput, Buffer, 8, &NumberOfCharsRead, NULL);
-    v0 = sub_401370(Buffer);
-    ExitProcess(v0);
-
-
+    //Read the Key from the Console. The key has a length of 8 caracters
+    ReadConsoleA(hConsoleInput, Key, 8, &NumberOfCharsRead, NULL);
+    //Decrypt all the files
+    uExitCode = Ransom_DecryptFiles(Key);
+    //Exit the process
+    ExitProcess(uExitCode);
 }
